5de697f0fc0fd63ebb3dd5a1afe68da659a341ff
arth1312 <arthkoradiya@gmail.com>
2025-03-12 18:26:35 +0530

    01_basic 12/03/2025

---
+++ 01_basic/basic.js
@@ -0,0 +1,11 @@
+// 01. consol.log()
+// console.log("Arth");
+
+// 02. alert()
+// alert("Arth koradiya")
+
+// 03. document.write()
+// document.write("Arth")
+
+// 04. DOM (Document object model)
+document.getElementById("name").textContent = "My name is Arth Koradiya"
