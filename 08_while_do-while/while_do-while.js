6dc6f0a19d68ea73bc9194e48dc5e3a093877eaf
arth1312 <arthkoradiya@gmail.com>
2025-03-28 18:47:33 +0530

    08 while_do-while 28/03/2025

---
+++ 08_while_do-while/while_do-while.js
@@ -0,0 +1,52 @@
+// Q-1
+// console.log("print out series 1,4,9,16,...,n");
+// let m = +prompt("Enter a number:");
+// let i = 1;
+// while(i <= m)
+// {
+//     console.log(i * i);
+//     i++;
+// }
+
+
+// Q-2
+// console.log("print out series 1,4,3,16,5,...,n");
+// let n = +prompt("Enter a number:");
+// let i = 1;
+// do
+// {
+//     console.log(i);
+//     i = i + 3;
+// }while(i <= n)
+
+// while(i <= n)
+// {
+//     console.log(i);;
+//     i = i + 3;
+// }
+
+
+// Q-3
+// console.log("print out series 1,2,4,8,16,...,n");
+// let o = +prompt("Enter a number:");
+// let i = 1;
+// while(i <= o)
+// {
+//     console.log(i * i);
+//     i++;
+// }
+
+
+// Q-4
+// console.log("print out series 1,4,12,32,80,...,n");
+
+
+// Q-5
+// console.log("find the sum of numbers from 1 to 100");
+// let sum = 0;
+// for(let i = 1; i <= 100; i++)
+// {
+//     sum += i;
+// }
+// console.log("Sum:", sum);
+
