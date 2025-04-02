eae56dd134d3b6d3b0b05fab229d07f727d11d76
arth1312 <arthkoradiya@gmail.com>
2025-04-02 18:46:03 +0530

    10 function 02/04/2025

---
+++ 10_function/function.js
@@ -0,0 +1,137 @@
+// Function => Perform to specific task
+
+// 1. find two value of min number
+{
+    // function minNumber()
+    // {
+    //     let a = 251, b = 50;
+    //     if(a < b)
+    //     {
+    //         console.log("min Number:", a);
+    //     }
+    //     else
+    //     {
+    //         console.log("min Number:", b);
+    //     }
+    // }
+    // minNumber();
+}
+
+// 2. two number swapping
+{
+    // function swapping()
+    // {
+    //     let a = 50, b = 13, c;
+    //     console.log("Value of a:", a);
+    //     console.log("Value of b:", b);
+    //     c = a;
+    //     a = b;
+    //     b = c;
+    //     console.log("Swap Value of a:", a);
+    //     console.log("Swap Value of b:", b);
+    // }
+    // swapping();
+}
+
+// 3. calculate area of circle
+{
+    // function areaCircle()
+    // {
+    //     let r = 20;
+    //     let area_circle;
+    //     area_circle = 3.14 * (r ** 2)
+    //     console.log("Area of circle is:", area_circle);
+    // }
+    // areaCircle();
+}
+
+// 4. calculate area of square
+{
+    // function areaSquare()
+    // {
+    //     let a = 10;
+    //     let area_square;
+    //     area_square = a * a;
+    //     console.log("Area of square is:", area_square);
+    // }
+    // areaSquare();
+}
+
+// 5. calculate area of triangle
+{
+    // function areaTriangle()
+    // {
+    //     let a = 30;
+    //     let b = 15;
+    //     let area_triangle;
+    //     area_triangle = a * b / 2;
+    //     console.log("Area of triangle is:", area_triangle);
+    // }
+    // areaTriangle();
+}
+
+// 6. calculate area of rectangle
+{
+    // function hoisting
+    // areaRectangle();
+    // function areaRectangle()
+    // {
+    //     let a = 15;
+    //     let b = 20;
+    //     let area_rectangle;
+    //     area_rectangle = a * b;
+    //     console.log("Area of rectangle is:", area_rectangle);
+    // }
+}
+
+// 7. find its positive or negative.
+{
+    // function hoisting
+    // positiveNegative();
+    // function positiveNegative()
+    // {
+    //     let a = +prompt("Enter your number:");
+    //     if(a > 0)
+    //     {
+    //         console.log("Positive number")
+    //     }
+    //     else
+    //     {
+    //         console.log("Negative number")
+    //     }
+    // }
+}
+
+// 8. divisible by 5 and 3 or not
+{
+    // function divisibleNot()
+    // {
+    //     let a = 40;
+    //     if(a % 5 === 0 && a % 3 === 0)
+    //     {
+    //         console.log("number is divisible by 5 and 3");
+    //     }
+    //     else
+    //     {
+    //         console.log("number is not divisible by 5 and 3");
+    //     }
+    // }
+    // divisibleNot();
+}
+
+// 9. find leap year or not
+{
+    // function leapYear()
+    // {
+    //     let year = 2025;
+    //     if(year % 4 === 0)
+    //     {
+    //         console.log(year, "is leap year");
+    //     }
+    //     else
+    //     {
+    //         console.log(year, "is not leap year");
+    //     }
+    // }
+    // leapYear();
+}
