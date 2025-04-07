46db4d6b94e9d03d29eae65de28b425494e389c3
arth1312 <arthkoradiya@gmail.com>
2025-04-07 18:26:45 +0530

    13 nested-function 07/04/2025

---
+++ 13_nested-function/nested-function.js
@@ -0,0 +1,46 @@
+// nested function
+
+// function greet()
+// {
+//     console.log("Hello guys...");
+
+//     function hello()
+//     {
+//         console.log("Good afternoon...");
+//     }
+//     hello();
+// }
+// greet();
+
+// laxical scope
+
+// function greet(n)
+// {
+//     console.log("Hello guys...");
+
+//     function hello()
+//     {
+//         console.log("Good afternoon...", n);
+//     }
+//     hello();
+// }
+// greet(122);
+
+// clouser concept
+
+// variable is a function(anonymous function)
+// let a = function ()
+// {
+//     console.log("Arth");
+// }
+// a();
+
+// Arrow function
+// let add = (a) => ("Arth koradiya");
+// console.log(add(12));
+
+// IIFE
+// (function ()
+// {
+//     console.log("IIFE");
+// })();
