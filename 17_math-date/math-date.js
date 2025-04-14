7a320aba2cfc60ba9eef187a436fd1540053a74d
arth1312 <arthkoradiya@gmail.com>
2025-04-14 18:44:34 +0530

    17 math-data

---
+++ 17_math-date/math-date.js
@@ -0,0 +1,31 @@
+// Date Method:-
+
+
+// let data = new Date();
+
+// let data = new Date("2020-03-21");
+
+// let data = new Date(2022, 5, 35, 22, 30, 23, 1500);
+
+let data = new Date("2020-03-21 10:06:00");
+console.log(data);
+
+// data.setDate(15)
+// data.setFullYear(2022)
+// data.setHours(5, 4, 55)
+// data.setMilliseconds(165)
+// data.setMinutes(10)
+// data.setMonth(4)
+// data.setSeconds(55)
+// data.setTime(55)
+// data.setUTCDate(22)
+// data.setUTCFullYear(2025)
+// data.setUTCHours(24)
+// data.setUTCMilliseconds(15000)
+// data.setUTCMinutes(10)
+// data.setUTCMonth(3)
+// data.setUTCSeconds(33)
+// console.log(data);
+
+data.getDate()
+console.log(data);
