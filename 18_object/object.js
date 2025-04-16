4aa2a2308da47bcc21e80a0b5edfd1ef69e88186
arth1312 <arthkoradiya@gmail.com>
2025-04-16 18:31:15 +0530

    18 object 16/04/2025

---
+++ 18_object/object.js
@@ -0,0 +1,54 @@
+// Object:- Everthing is object
+// define:- key: value(string, number, boolen, null, undefined, function)
+
+// let data = new Object()
+
+// let data = {}
+// console.log(data);
+// console.log(typeof data);
+
+let data = {
+    name: "Arth Koradiya",
+    age: 25,
+    email: 'abc@test.in',
+    indian: true,
+    sayHello: function ()
+              {
+                console.log("Hello All");
+              }
+}
+console.log(data);
+
+
+// Access two method:-
+
+// 1. Dot(.) method:-
+console.log(data.name);
+console.log(data.age);
+console.log(data.email);
+console.log(data.indian);
+data.sayHello()
+
+// ['key'] method:-
+console.log(data['name']);
+console.log(data['age']);
+console.log(data['email']);
+console.log(data['indian']);
+
+data.name = "Rohit Sharma";
+data['hello'] = 100;
+console.log(data);
+
+delete data.age;
+console.log(data);
+
+// Main method:-
+
+// 1. hasOwnProperty
+console.log(data.hasOwnProperty('name'));
+
+// 2. key
+console.log(Object.keys(data));
+
+// 3. values
+console.log(Object.values(data));
