5bb88d1eb44d9cc0abb1dcdfccfbcb666deaba00
arth1312 <arthkoradiya@gmail.com>
2025-05-08 18:29:41 +0530

    31 JSON 08/05/2025

---
+++ 31_json/json.js
@@ -0,0 +1,38 @@
+// JSON:- JavaScript Object Notation
+
+// JavaScript Object
+// let person = {
+//     name : "Rohit Sharma",
+//     age : 35,
+//     email : "rohit@test.in"
+// }
+// console.log(person);
+
+
+// JSON Object
+// let data = `{
+//     name : "Virat Kohli",
+//     age : 30,
+//     email : "virat@test.in"
+// }`
+// console.log(data);
+
+
+// JavaScript convert to JSON
+// let person = {
+//     name : "Rohit Sharma",
+//     age : 35,
+//     email : "rohit@test.in"
+// }
+// console.log(person);
+
+// let data = JSON.stringify(person);
+// console.log(data);
+
+
+// JSON convert to JavaScript
+// let data = `{"name" : "Virat Kohli","age" : 30,"email" : "virat@test.in"}`
+// console.log(data);
+
+// let person1 = JSON.parse(data);
+// console.log(person1);
