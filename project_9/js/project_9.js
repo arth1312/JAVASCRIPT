002ba0013a4a7c3fb42dc539280dfa6a1b6fe88c
arth1312 <arthkoradiya@gmail.com>
2025-05-15 18:40:37 +0530

    project_9 15/05/2025

--- project_9/js/project_9.js
+++ project_9/js/project_9.js
@@ -1,52 +1,65 @@
-// Add to Cart
+let ProductForm = document.querySelector(".ProductForm");
+let ProductName = document.querySelector("#Product_name");
+let ProductImage = document.querySelector("#Product_image");
+let ProductPrice = document.querySelector("#Product_price");
+let ProductDescription = document.querySelector("#Product_description");
 
-let Product_Form = document.querySelector("#ProductForm");
-let Product_Name = document.querySelector("#Product_name");
-let Product_Image = document.querySelector("#Product_image");
-let Product_Price = document.querySelector("#Product_price");
-let Product_description = document.querySelector("#Product_description");
-
-Product_Form.addEventListener('submit', (e) => {
+ProductForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let products = JSON.parse(sessionStorage.getItem('products')) || [];
+    let products = JSON.parse(localStorage.getItem('products')) || [];
 
     let formData = {
-        Product_name: Product_Name.value,
-        Product_image: Product_Image.value,
-        Product_price: Product_Price.value,
-        Product_description: Product_description.value
-    }
+        Product_name: ProductName.value,
+        Product_image: ProductImage.value,
+        Product_price: ProductPrice.value,
+        Product_description: ProductDescription.value
+    };
 
-    products.push(formData)
-
+    products.push(formData);
     localStorage.setItem('products', JSON.stringify(products));
+
     displayProduct();
-    Product_Form.reset();
+
+    ProductForm.reset();
 });
 
 function displayProduct() {
-    let products = JSON.parse(localStorage.getItem('products'));
+    let products = JSON.parse(localStorage.getItem('products')) || [];
 
     let rowData = "";
     products.forEach((record, index) => {
         let row = `
             <div class="product-cards" data-index="${index}">
-                <div class="product-card__image"> 
-                    <img src="${record.Product_image}" alt="${record.Product_name}" width="100">
+                <div class="product-card__image">
+                    <img src="${record.Product_image}" alt="${record.Product_name}">
                 </div>
                 <div class="product-card__info">
-                    <h2 class="product-card__title">${record.Product_name}</h2>
+                    <h5 class="product-card__title">${record.Product_name}</h5>
                     <p class="product-card__description">${record.Product_description}</p>
                     <div class="product-card__price-row">
-                        <span class="product__price">${record.Product_price}</span>
-                        <button href="#" class="Product-card__btn">Add to Cart</button>
+                        <span class="product-card__price">${record.Product_price}</span>
+                        <button class="Product-card__btn">Add to Cart</button>
                     </div>
                 </div>
             </div>
         `;
         rowData = rowData + row;
-    })
-};
-document.querySelector(".cont").innerHTML = rowData;
+    });
+
+    document.querySelector(".cont").innerHTML = rowData;
+    document.querySelectorAll(".Product-card__btn").forEach((btn, index) => {
+        btn.addEventListener("click", () => addToCart(index))
+    });
+}
+
+function addToCart(index) {
+    let products = JSON.parse(localStorage.getItem('Product')) || [];
+    let cartProducts = JSON.parse(localStorage.getItem('CartProducts')) || [];
+    let productToAdd = products[index];
+    cartProducts.push(productToAdd);
+    localStorage.setItem('CartProducts', JSON.stringify(cartProducts));
+    alert(`${productToAdd.Product_name} added to cart`);
+}
+
 displayProduct();
