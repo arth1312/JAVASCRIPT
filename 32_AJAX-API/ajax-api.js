6f54e83306766890d018d1334015b0b9df1c8e28
arth1312 <arthkoradiya@gmail.com>
2025-05-21 18:40:36 +0530

    32 AJAX-API 21/05/2025

---
+++ 32_AJAX-API/ajax-api.js
@@ -0,0 +1,19 @@
+// AJAX :- Asynchronous JavaScript with XML
+
+document.querySelector("button").addEventListener("click", () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xhr.onload = function() {
+        // console.log(this.responseText);
+        // document.writeln(this.responseText);
+
+        let div = document.getElementById("test");
+        let data = JSON.parse(this.responseText);
+        data.forEach((user) => {
+            let li = document.createElement("li");
+            li.innerHTML = user.title;
+            div.append(li);
+        });
+    };
+    xhr.send();
+});
