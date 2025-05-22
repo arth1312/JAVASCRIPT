7164c09fc2f151dee2dd6b9e27ba67f2c1421857
arth1312 <arthkoradiya@gmail.com>
2025-05-22 18:37:24 +0530

    34 Dog-API_diffrent

---
+++ 34_Dog-API_diffrent/dog-api_diffrent.js
@@ -0,0 +1,36 @@
+let select = document.querySelector('select');
+
+fetch("https://dog.ceo/api/breeds/list/all")
+.then(res => res.json())
+.then(data => {
+    let breedList = Object.keys(data.message);
+
+    breedList.forEach(breed => {
+      let option = document.createElement('option');
+      option.innerHTML = breed;
+      option.setAttribute('value', breed);
+      select.append(option);
+    });
+})
+.catch(err => console.log(err));
+
+document.querySelector("button").addEventListener("click", () => {
+    let breedList = select.value;
+
+    fetch(`https://dog.ceo/api/breed/${breedList}/images`)
+        .then(res => res.json())
+        .then(data => {
+            let dogList = data.message;
+            document.querySelector("#test").innerHTML = "";
+
+            dogList.forEach(dog => {
+                let img = document.createElement('img');
+                img.setAttribute('src', dog);
+                img.setAttribute('width', "200px");
+                img.setAttribute('height', "200px");
+                img.style.padding = "5px";
+                document.querySelector("#test").prepend(img);
+            });
+        })
+    .catch(err => console.log(err));
+});
