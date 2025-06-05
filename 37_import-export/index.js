b2cfeb9f06179ea99f7c430f6e62115b880846c0
arth1312 <arthkoradiya@gmail.com>
2025-06-05 18:29:54 +0530

    37 import-export 05/06/2025

---
+++ 37_import-export/index.js
@@ -0,0 +1,7 @@
+// const { product } = require('./test');
+
+// console.log(product(12));
+
+import product from './test.js';
+
+console.log(product(12));
---
+++ 37_import-export/test.js
@@ -0,0 +1,9 @@
+// function product(n) {
+//     return n * n;
+// }
+
+// module.exports = { product };
+
+export default function product(n) {
+    return n * n;
+}
